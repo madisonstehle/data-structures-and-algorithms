056bda32872ef92425521046015fccdab677fc29
Madison Stehle <mnstehle3@gmail.com>
2020-01-27 11:48:58 -0800

    passed test 4

--- code-challenges/filter/challenges-08.test.js
+++ code-challenges/filter/challenges-08.test.js
@@ -87,7 +87,13 @@ const snorlaxData = {
 };
 
 const getBaseStatGreaterThan = (arr, minBaseStat) => {
-  // Solution code here...
+  return arr.filter( obj => {
+    if (obj.baseStat > minBaseStat){
+      return obj;
+    } else {
+      return null;
+    }
+  });
 };
 
 /* ------------------------------------------------------------------------------------------------
@@ -224,7 +230,7 @@ describe('Testing challenge 3', () => {
   });
 });
 
-xdescribe('Testing challenge 4', () => {
+describe('Testing challenge 4', () => {
   test('It should return an array containing the stats that are greater than the input', () => {
     expect(getBaseStatGreaterThan(snorlaxData.stats, 75)).toStrictEqual([ { stat: { url: 'https://pokeapi.co/api/v2/stat/5/', name: 'special-defense' }, effort: 2, baseStat: 110 } ]);
     expect(getBaseStatGreaterThan(snorlaxData.stats, 75).length).toStrictEqual(1);
