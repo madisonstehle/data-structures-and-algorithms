e552c486e5c0d4c6a3e2eac47e60c264d9159977
Madison Stehle <mnstehle3@gmail.com>
2020-02-02 15:22:09 -0800

    passed test 5

--- code-challenges/reduce/challenges-09.test.js
+++ code-challenges/reduce/challenges-09.test.js
@@ -159,7 +159,16 @@ Hint: The accumulator should begin as { count: 0, sum: 0 }
 ------------------------------------------------------------------------------------------------ */
 
 const calculateAverage = (arr) => {
-  // Solution code here...
+  let avg = 0;
+
+  arr.reduce( (acc, val) => {
+    acc.sum = acc.sum + val;
+    ++acc.count;
+    avg = acc.sum / acc.count;
+    return { count: acc.count, sum: acc.sum };
+  }, { count: 0, sum: 0 });
+
+  return avg;
 };
 
 /* ------------------------------------------------------------------------------------------------
@@ -275,7 +284,7 @@ describe('Testing challenge 4', () => {
   });
 });
 
-xdescribe('Testing challenge 5', () => {
+describe('Testing challenge 5', () => {
   test('It should return the average of the numbers in the array', () => {
     expect(calculateAverage([18, 290, 37, 4, 55, 16, 7, 85 ])).toStrictEqual(64);
   });
