b8eed40012b30b14a02122778c9bb86b48e40b52
Madison Stehle <mnstehle3@gmail.com>
2020-03-23 08:47:10 -0700

    initialized binary search challenge

---
+++ challenges/arrayBinarySearch/array-binary-search.js
@@ -0,0 +1,19 @@
+'use strict';
+
+// Write a function called BinarySearch which takes in 2 parameters: a sorted array and the search key. Without utilizing any of the built-in methods available to your language, return the index of the array’s element that is equal to the search key, or -1 if the element does not exist.
+
+let InputArrayOne = [4,8,15,16,23,42];
+let InputSearchOne = 15;
+
+let InputArrayTwo = [11,22,33,44,55,66,77];
+let InputSearchTwo = 90;
+
+function BinarySearch(array, searchVal) {
+  // code here
+};
+
+// BinarySearch(InputArrayOne, InputSearchOne);
+// BinarySearch(InputArrayTwo, InputSearchTwo);
+
+// outputOne = 2;
+// outputTwo = -1;
