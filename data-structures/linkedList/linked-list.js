3453a00fda714a1d32d164b43307b0d679c58477
Madison Stehle <mnstehle3@gmail.com>
2020-03-31 20:20:42 -0700

    added scaffold for code challenge 6

--- data-structures/linkedList/linked-list.js
+++ data-structures/linkedList/linked-list.js
@@ -68,6 +68,18 @@ class LinkedList {
       console.error('ERROR PRINTING LIST')
     }
   }
+
+  append(){
+    // which adds a new node with the given value to the end of the list
+  }
+
+  insertBefore(value, newVal){
+    // which add a new node with the given newValue immediately before the first value node
+  }
+
+  insertAfter(value, newVal){
+    // which add a new node with the given newValue immediately after the first value node
+  }
 };
 
 module.exports = { Node, LinkedList };
