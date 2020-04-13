2d38bb9c3a3899a3729d53a4b388242927547478
Madison Stehle <mnstehle3@gmail.com>
2020-04-13 16:19:12 -0700

    initialized code challenge queueWithStacks files

---
+++ challenges/queueWithStacks/queue-with-stacks.js
@@ -0,0 +1,25 @@
+'use strict';
+
+
+class PseudoQueue {
+  enqueue(value) {
+    try {
+
+    }
+    catch(e) {
+
+    }
+  }
+
+  dequeue() {
+    try {
+
+    }
+    catch(e) {
+
+    }
+  }
+}
+
+
+module.exports = PseudoQueue;
---
+++ challenges/queueWithStacks/queue-with-stacks.test.js
@@ -0,0 +1,21 @@
+'use strict';
+
+let PseudoQueue = require('./queue-with-stacks.js');
+
+describe('Happy Path', () => {
+  it('to do something', () => {
+
+  });
+});
+
+describe('Expected Failure', () => {
+  it('to do something', () => {
+
+  });
+});
+
+describe('Edge Cases', () => {
+  it('to do something', () => {
+
+  });
+});
