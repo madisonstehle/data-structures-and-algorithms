57828f2356fd8c832a5c97945275d12edb78270e
Madison Stehle <mnstehle3@gmail.com>
2020-04-24 22:03:52 -0700

    dequeue method works

--- data-structures/fifoAnimalShelter/fifo-animal-shelter.js
+++ data-structures/fifoAnimalShelter/fifo-animal-shelter.js
@@ -27,12 +27,38 @@ class AnimalShelter {
   }
 
   dequeue(pref){
+    if (!this.front && !this.rear) return console.log('ERROR: List is Empty!');
 
+    if (this.front.animal === pref){
+      let dequeuedNode = this.front;
+      this.front = this.front.next;
+      dequeuedNode.next = null;
+      return dequeuedNode;
+    }
+
+    let currentNode = this.front;
+    let nextNode = this.front.next;
+    let prevNode = null;
+
+    while(currentNode.animal !== pref){
+      prevNode = currentNode;
+      currentNode = currentNode.next;
+      nextNode = currentNode.next;
+    }
+    prevNode.next = nextNode;
+    currentNode.next = null;
+    return currentNode;
   }
 }
 
 let shelter = new AnimalShelter();
+
+shelter.enqueue('dog');
+shelter.enqueue('dog');
 shelter.enqueue('dog');
 shelter.enqueue('cat');
 shelter.enqueue('cat');
 shelter.enqueue('dog');
+
+shelter.dequeue('dog');
+shelter.dequeue('cat');
