c1a162ff2ae1c9e09607c68ebe389f1c65ccf3e8
Madison Stehle <mnstehle3@gmail.com>
2020-04-28 09:58:59 -0700

    braacket validation function and testing

--- README.md
+++ README.md
@@ -48,5 +48,5 @@ Code Fellows 301n16 & 401n16
   - Round Brackets : `()`
   - Square Brackets : `[]`
   - Curly Brackets : `{}`
-- **Approach & Efficiency**: 
+- **Approach & Efficiency**: I had to do a lot of research with this one. [This article](https://medium.com/@paulrohan/parenthesis-matching-problem-in-javascript-the-hacking-school-hyd-7d7708278911) on Medium and [this article](https://js-algorithms.tutorialhorizon.com/2015/11/16/justify-if-a-string-consists-of-valid-parentheses/) on TutorialHorizon helped me to better understand the requirements of this problem. I knew that I would have to have some if statements and retain some sort of record of the brackets of the input string. There were a couple ideas that came to mind. I could see this working by creating a "bracket array" and pushing into that the brackets of the input string, kind of like how a stack works. In the end I decided to use the `.reduce()` method. I brushed up on `.reduce()` by checking out the [MDN docs](https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/Array/reduce). The efficiency should be O(n), or linear time.
 - **Solution**: ![Multi-Bracket Validation Whiteboard](multi-bracket-validation-whiteboard.PNG)
--- challenges/multiBracketValidation/multi-bracket-validation.test.js
+++ challenges/multiBracketValidation/multi-bracket-validation.test.js
@@ -3,19 +3,55 @@
 let multiBracketValidation = require('./multi-bracket-validation.js');
 
 describe('happy path', () => {
-  it('to do a thing', () => {
+  it('validate a pair of brackets', () => {
+    let response = multiBracketValidation('{}');
 
+    expect(response).toBe(true);
+  });
+
+  it('validate multiple pairs of brackets', () => {
+    let response = multiBracketValidation('{}(){}');
+
+    expect(response).toBe(true);
+  });
+
+  it('validate brackets with other characters nested', () => {
+    let response = multiBracketValidation('()[[Extra Characters]]');
+
+    expect(response).toBe(true);
+  });
+
+  it('validate nested brackets', () => {
+    let response = multiBracketValidation('(){}[[]]');
+
+    expect(response).toBe(true);
+  });
+
+  it('validate multiple other characters within different sets of paired brackets', () => {
+    let response = multiBracketValidation('{}{Code}[Fellows](())');
+
+    expect(response).toBe(true);
   });
 });
 
 describe('expected failure', () => {
-  it('to do a thing', () => {
+  it('invalid: no closing (', () => {
+    let response = multiBracketValidation('[({}]');
 
+    expect(response).toBe(false);
+  });
+
+  it('invalid: no matching brackets', () => {
+    let response = multiBracketValidation('(](');
+
+    expect(response).toBe(false);
   });
 });
 
 describe('edge cases', () => {
-  it('to do a thing', () => {
+  it('validate a string with no brackets as true', () => {
+    let response = multiBracketValidation('sdfjgobnwoij2498dfj');
 
+    expect(response).toBe(true);
   });
 });
