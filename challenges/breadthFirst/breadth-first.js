7e4c2ed6863005213131694210e66e6c9b40c5d0
Madison Stehle <mnstehle3@gmail.com>
2020-05-15 20:17:33 -0700

    breadadth first code

--- challenges/breadthFirst/breadth-first.js
+++ challenges/breadthFirst/breadth-first.js
@@ -1,15 +1,46 @@
 'use strict';
 
 const Node = require('../../data-structures/tree/tree.js').Node;
+const Queue = require('../../data-structures/queueWithStacks/queue.js');
 
 class BinaryTree {
   constructor() {
     this.root = null;
   }
 
-  breadthFirst() {
+  breadthFirst(root) {
+    if (!root) return false;
+
+    let nodeArray = [];
+
+    let queue = new Queue();
+    queue.enqueue(root);
+
     
+    while (!queue.isEmpty()) {
+      console.log('====== WHILE ======');
+      console.log('QUEUE: ', queue);
+      
+      nodeArray.push(queue.dequeue().val);
+      
+      if (root.left) queue.enqueue(root.left);
+      if (root.right) queue.enqueue(root.right);
+    }
+    console.log(nodeArray);
+
+    return nodeArray;
   }
 }
+
+let myTree = new BinaryTree();
+myTree.root = new Node(5);
+myTree.root.left = new Node(3);
+myTree.root.right = new Node(7);
+myTree.root.left.left = new Node(2);
+myTree.root.left.right = new Node(4);
+myTree.root.right.left = new Node(6);
+myTree.root.right.right = new Node(8);
+
+myTree.breadthFirst(myTree.root);
 
 module.exports = BinaryTree;
