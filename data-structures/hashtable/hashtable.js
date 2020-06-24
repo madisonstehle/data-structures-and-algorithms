a1aafbfba847ae2ab028ada7c6fbdd5522a1f135
Madison Stehle <mnstehle3@gmail.com>
2020-06-23 19:51:22 -0700

    wrote tests

--- data-structures/hashtable/hashtable.js
+++ data-structures/hashtable/hashtable.js
@@ -2,40 +2,83 @@
 
 
 class HashTable {
-  constructor() {
-    this.table = new Array(137);
+  constructor(size) {
+    this.table = new Array(size);
     this.values = [];
   }
 
-  hash(string) {
-    const H = 37;
-    let total = 0;
-    for (var i = 0; i< string.length; i++) {
-      total += H * total + string.charCodeAt(i);
-    }
-    total %= this.table.length;
-    if (total <1) {
-      this.table.length -1
-    }
-    return parseInt(total);
-  }
-
   add(key, value) {
-    const pos = this.hash(key);
-    this.table[pos] = {key: value};
+    let addIdx = this.hash(key);
+
+    if (this.table[addIdx]) {
+      let current = this.table[addIdx];
+
+      while(current.next) {
+        if (current.key === key) {
+          if (current.val.length) {
+            current.val.push(value);
+          } else {
+            current.val = [current.val, value];
+          }
+          return;
+        }
+        current = current.next;
+      }
+
+      current.next = {
+        key: key,
+        val: value
+      }
+    } else {
+      this.table[addIdx] = {
+        key: key,
+        val: value
+      }
+    }
+    
+
   }
 
   get(key) {
-    const pos = this.hash(key);
-    let index = 0;
-    while (this.table[pos][index] !=  key){
-      if(this.table[pos][index] !== undefined){
-        return this.table[pos][index]
-      } else return undefined; 
+    let idx = this.hash(key);
+
+    if (this.table[idx]) {
+      let current = this.table[idx];
+
+      while (current) {
+        if (current.key === key) {
+          return current.val;
+        } 
+        current = current.next;
+      } 
     }
-    index ++
+
+    return null;
   }
 
+  contains(key) {
+    let val = this.get(key);
+
+    if (!val) return false;
+    else return true;
+  }
+
+  hash(key) {
+    const H = 599;
+    let total = 0;
+
+    for (var i = 0; i< key.length; i++) {
+      total += H * (total + key.charCodeAt(i));
+    }
+
+    total %= this.table.length;
+
+    if (total <1) {
+      this.table.length -1
+    }
+
+    return parseInt(total);
+  }
 };
 
 
