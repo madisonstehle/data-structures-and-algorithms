afffdf74d3aac32d26d5bf65178a5d577b98acbc
Madison Stehle <mnstehle3@gmail.com>
2020-06-27 07:16:59 -0700

    initialized files, wrote tests

---
+++ challenges/repeatedWord/repeatedWord.js
@@ -0,0 +1,7 @@
+'use strict';
+
+const repeatedWord = (str) => {
+
+}
+
+module.exports = repeatedWord;
---
+++ challenges/repeatedWord/repeatedWord.test.js
@@ -0,0 +1,23 @@
+'use strict';
+
+const repeatedWord = require('./repeatedWord.js');
+
+describe('Happy Path', () => {
+  it('smol word', () => {
+    let response = repeatedWord('Once upon a time, there was a brave princess who...');
+
+    expect(response).toBe('a')
+  });
+
+  it('long passage', () => {
+    let response = repeatedWord('It was the best of times, it was the worst of times, it was the age of wisdom, it was the age of foolishness, it was the epoch of belief, it was the epoch of incredulity, it was the season of Light, it was the season of Darkness, it was the spring of hope, it was the winter of despair, we had everything before us, we had nothing before us, we were all going direct to Heaven, we were all going direct the other way – in short, the period was so far like the present period, that some of its noisiest authorities insisted on its being received, for good or for evil, in the superlative degree of comparison only...');
+
+    expect(response).toBe('it')
+  });
+
+  it('long word', () => {
+    let response = repeatedWord('It was a queer, sultry summer, the summer they electrocuted the Rosenbergs, and I didn’t know what I was doing in New York...');
+
+    expect(response).toBe('summer')
+  });
+});
